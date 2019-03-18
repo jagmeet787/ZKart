ab128f4d4cf66f2afca163f6a37f399d97179fed
Jagmeet Singh <jagmeet787@gmail.com>
2019-03-18 17:47:00 +0530

    Initial Commit.

---
+++ zkart-api/src/main/java/zkart/ZkartApiApplication.java
@@ -0,0 +1,13 @@
+package zkart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkartApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZkartApiApplication.class, args);
+	}
+
+}
