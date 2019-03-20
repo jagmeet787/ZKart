bfa3481c9b442d8bbfa7174c249a086cbfcdc13d
kh-satyam <kh.satyam09@gmail.com>
2019-03-20 17:48:57 +0530

    categories API completed

--- zkart-api/src/main/java/zkart/controller/CategoryController.java
+++ zkart-api/src/main/java/zkart/controller/CategoryController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import zkart.entity.Category;
@@ -28,6 +30,33 @@ public class CategoryController {
 	@RequestMapping("/{id}")
 	public ResponseEntity<Category> getZkartCategories(@PathVariable("id") int id){
 		return new ResponseEntity<>(categoryService.getZkartCategoryById(id),HttpStatus.OK);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/create")
+	public ResponseEntity<String> addZkartCategory(@RequestBody Category category){
+		if(categoryService.addZkartCategory(category)==true) {
+			return new ResponseEntity<>("category added",HttpStatus.OK);
+		}else {
+			return new ResponseEntity<>("error",HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT, value = "/update/{id}")
+	public ResponseEntity<String> updateZkartCategory(@PathVariable("id") Integer id,@RequestBody Category category){
+		if(categoryService.updateZkartCategory(category,id)==true) {
+			return new ResponseEntity<>("category updated",HttpStatus.OK);
+		}else {
+			return new ResponseEntity<>("error",HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@RequestMapping(method = RequestMethod.DELETE, value = "/delete/{id}")
+	public ResponseEntity<String> updateZkartCategory(@PathVariable("id") Integer id){
+		if(categoryService.deleteZkartCategory(id)==true) {
+			return new ResponseEntity<>("category deleted",HttpStatus.OK);
+		}else {
+			return new ResponseEntity<>("error",HttpStatus.BAD_REQUEST);
+		}
 	}
 
 }
--- zkart-api/src/main/java/zkart/service/CategoryService.java
+++ zkart-api/src/main/java/zkart/service/CategoryService.java
@@ -27,4 +27,38 @@ public class CategoryService {
 	public Category getZkartCategoryById(Integer id) {
 		return categoryRepository.findById(id).get();
 	}
+	
+	public boolean addZkartCategory(Category category) {
+		boolean res=true;
+		try {
+			categoryRepository.save(category);
+		}catch(Exception e) {
+			res=false;
+			System.out.println(e);
+		}
+		return res;
+	}
+	
+	public boolean updateZkartCategory(Category category,Integer id) {
+		category.setId(id);
+		boolean res=true;
+		try {
+			categoryRepository.save(category);
+		}catch(Exception e) {
+			res=false;
+			System.out.println(e);
+		}
+		return res;
+	}
+	
+	public boolean deleteZkartCategory(Integer id) {
+		boolean res=true;
+		try {
+			categoryRepository.deleteById(id);
+		}catch(Exception e) {
+			res=false;
+			System.out.println(e);
+		}
+		return res;
+	}
 }
