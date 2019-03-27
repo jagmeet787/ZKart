0137ff4e998e1e2f7156682488acf3d0833b432d
wompowe <14103022utkarsh@gmail.com>
2019-03-27 05:34:19 +0530

    not relevant

--- zkart-api/src/main/java/zkart/controller/OrdersController.java
+++ zkart-api/src/main/java/zkart/controller/OrdersController.java
@@ -28,7 +28,7 @@ import zkart.service.UserService;
 @RequestMapping("/orders")
 public class OrdersController {
 
-	static final Integer ZKART_USERID = 1;
+	static final Integer ZKART_USERID = 26;
 	static final Integer RETURN_DAYS = 15;
 	
 	@Autowired
